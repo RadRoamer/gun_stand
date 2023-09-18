12f33a3e2af6b5e1c08cfd4b73cf245e456d907f
Vlad Zasypko <zasypkovlad209@gmail.com>
2023-09-18 11:58:08 +0300

    add main_loop function

--- main_module/main.py
+++ main_module/main.py
@@ -1,4 +1,26 @@
+import pygame as pg
+from pygame.locals import *
+
+WIDTH = 800
+HEIGHT = 600
+FPS = 60
+
+
+def main_loop():
+    running = True
+
+    screen = pg.display.set_mode((WIDTH, HEIGHT), RESIZABLE)
+    clock = pg.time.Clock()
+    while running:
+        for event in pg.event.get():
+            if event.type == QUIT:
+                running = False
+
+        screen.fill('white')
+        pg.display.update()
+        clock.tick(FPS)
+    pg.quit()
 
 
 if __name__ == '__main__':
-    pass
+    main_loop()
