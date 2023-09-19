0aaabb89c4fc361947480a0861e161a849c05891
Vlad Zasypko <zasypkovlad209@gmail.com>
2023-09-19 18:30:34 +0300

    create a simple image rotation using mouse

--- main_module/main.py
+++ main_module/main.py
@@ -1,4 +1,5 @@
 import pygame as pg
+import math
 from pygame.locals import *
 
 WIDTH = 800
@@ -7,18 +8,48 @@ FPS = 60
 
 
 def main_loop():
-    running = True
+    screen = pg.display.set_mode((WIDTH, HEIGHT))
+    center_x, center_y = screen.get_size()
+    center_x, center_y = center_x // 2, center_y // 2
 
-    screen = pg.display.set_mode((WIDTH, HEIGHT), RESIZABLE)
+    # Clear the screen
+    screen.fill('purple')
+
+    turret_original = pg.image.load('../machine-gun_icon.png').convert_alpha()
+    t_width, t_height = turret_original.get_size()
+    turret_original = pg.transform.scale(turret_original,
+                                         (t_width // 3, t_height // 3))
+
     clock = pg.time.Clock()
+
+    running = True
     while running:
         for event in pg.event.get():
             if event.type == QUIT:
                 running = False
 
-        screen.fill('white')
+        keys = pg.key.get_pressed()
+        if keys[K_ESCAPE]:
+            running = False
+
+        pos = pg.mouse.get_pos()
+
+        # calc the rotation angle
+        x_dist = pos[0] - center_x
+        y_dist = -(pos[1] - center_y)
+        angle = math.degrees(math.atan2(y_dist, x_dist))
+
+        turret = pg.transform.rotate(turret_original, angle - 10)
+        turret_rect = turret.get_rect(center=(center_x, center_y))
+
+        # Clear the screen
+        screen.fill('purple')
+        # update the main surface
+        screen.blit(turret, turret_rect)
+
         pg.display.update()
         clock.tick(FPS)
+
     pg.quit()
 
 
